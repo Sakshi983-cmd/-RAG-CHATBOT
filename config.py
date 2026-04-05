2479051ee5d1466eb685a8c7e4a851995714fd78
Sakshi Tiwari <tiwarishakshi318@gmail.com>
2026-04-05 20:48:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,14 +3,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-GROQ_MODEL = "llama-3.1-8b-instant"  
-EMBEDDING_MODEL = "all-MiniLM-L6-v2"  
-
-CHUNK_SIZE = 200     
-CHUNK_OVERLAP = 30     
-
+GROQ_MODEL = "llama-3.1-8b-instant"
+EMBEDDING_MODEL = "all-MiniLM-L6-v2"
+CHUNK_SIZE = 200
+CHUNK_OVERLAP = 30
 TOP_K_RESULTS = 4
-
 DATA_DIR = "data"
 CHUNKS_DIR = "chunks"
 VECTORDB_DIR = "vectordb"
